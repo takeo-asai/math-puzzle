c38e01f929e4247298bbb14ce812cce11c127216
Takeo Asai <spitson+github@gmail.com>
2016-02-13 17:31:53 +0900

    permutation & combination from ExSwift

---
+++ problems/array_ex.swift
@@ -0,0 +1,93 @@
+extension Array {
+    //  ExSwift
+    //
+    //  Created by pNre on 03/06/14.
+    //  Copyright (c) 2014 pNre. All rights reserved.
+    //
+    //  https://github.com/pNre/ExSwift/blob/master/ExSwift/Array.swift
+    //  https://github.com/pNre/ExSwift/blob/master/LICENSE
+    /**
+        - parameter length: The length of each permutation
+        - returns: All permutations of a given length within an array
+    */
+    func permutation (length: Int) -> [[Element]] {
+        if length < 0 || length > self.count {
+            return []
+        } else if length == 0 {
+            return [[]]
+        } else {
+            var permutations: [[Element]] = []
+            let combinations = combination(length)
+            for combination in combinations {
+                var endArray: [[Element]] = []
+                var mutableCombination = combination
+                permutations += self.permutationHelper(length, array: &mutableCombination, endArray: &endArray)
+            }
+            return permutations
+        }
+    }
+    private func permutationHelper(n: Int, inout array: [Element], inout endArray: [[Element]]) -> [[Element]] {
+        if n == 1 {
+            endArray += [array]
+        }
+        for var i = 0; i < n; i++ {
+            permutationHelper(n - 1, array: &array, endArray: &endArray)
+            let j = n % 2 == 0 ? i : 0
+            let temp: Element = array[j]
+            array[j] = array[n - 1]
+            array[n - 1] = temp
+        }
+        return endArray
+    }
+    func combination (length: Int) -> [[Element]] {
+        if length < 0 || length > self.count {
+            return []
+        }
+        var indexes: [Int] = (0..<length).reduce([]) {$0 + [$1]}
+        var combinations: [[Element]] = []
+        let offset = self.count - indexes.count
+        while true {
+            var combination: [Element] = []
+            for index in indexes {
+                combination.append(self[index])
+            }
+            combinations.append(combination)
+            var i = indexes.count - 1
+            while i >= 0 && indexes[i] == i + offset {
+                i--
+            }
+            if i < 0 {
+                break
+            }
+            i++
+            let start = indexes[i-1] + 1
+            for j in (i-1)..<indexes.count {
+                indexes[j] = start + j - i + 1
+            }
+        }
+        return combinations
+    }
+    /**
+        - parameter length: The length of each permutations
+        - returns: All of the permutations of this array of a given length, allowing repeats
+    */
+    func repeatedPermutation(length: Int) -> [[Element]] {
+        if length < 1 {
+            return []
+        }
+        var indexes: [[Int]] = []
+        indexes.repeatedPermutationHelper([], length: length, arrayLength: self.count, indexes: &indexes)
+        return indexes.map({ $0.map({ i in self[i] }) })
+    }
+    private func repeatedPermutationHelper(seed: [Int], length: Int, arrayLength: Int, inout indexes: [[Int]]) {
+        if seed.count == length {
+            indexes.append(seed)
+            return
+        }
+        for i in (0..<arrayLength) {
+            var newSeed: [Int] = seed
+            newSeed.append(i)
+            self.repeatedPermutationHelper(newSeed, length: length, arrayLength: arrayLength, indexes: &indexes)
+        }
+    }
+}
