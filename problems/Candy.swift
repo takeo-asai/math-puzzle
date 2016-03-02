0c6210767a726c02f07dddd86f5819f33517ec8c
Takeo Asai <spitson+github@gmail.com>
2016-03-02 19:21:57 +0900

    bin tree count remove

--- problems/Candy.swift
+++ problems/Candy.swift
@@ -11,18 +11,20 @@ class Candy {
   var value: Int
   var R: Candy?
   var L: Candy?
+  var parent: Candy?
   var count: Int
 
-  init(_ v: Int) {
+  init(_ v: Int, _ p: Candy? = nil) {
     self.value = v
     self.count = 1
+    self.parent = p
   }
   func insert(n: Int) {
     if n < self.value {
       if let l = self.L {
         l.insert(n)
       } else {
-        self.L = Candy(n)
+        self.L = Candy(n, self)
       }
     } else if n == self.value {
       self.count += 1
@@ -30,7 +32,7 @@ class Candy {
       if let r = self.R {
         r.insert(n)
       } else {
-        self.R = Candy(n)
+        self.R = Candy(n, self)
       }
     }
   }
@@ -40,7 +42,7 @@ class Candy {
         return l.contains(n)
       }
     } else if n == self.value {
-      return true
+      return self.count > 0
     } else {
       if let r = self.R {
         return r.contains(n)
@@ -59,19 +61,33 @@ class Candy {
       return left
     }
   }
+  func maximum() -> Candy {
+    if let r = self.R {
+      return r.maximum()
+    }
+    return self
+  }
+  func replace(tree: Candy, with: Candy?) {
+    if tree.value == self.R?.value {
+      self.R = with
+      with?.parent = self.R
+    } else if tree.value == self.L?.value {
+      self.L = with
+      with?.parent = self.L
+    }
+    tree.parent = nil
+  }
   func remove(n: Int) {
-    if self.contains(n) {
-
+    if n < self.value {
+      if let l = self.L {
+        l.remove(n)
+      }
+    } else if n == self.value {
+      self.count -= 1
+    } else {
+      if let r = self.R {
+        r.remove(n)
+      }
     }
   }
 }
-
-var c = Candy(10)
-c.insert(16)
-c.insert(19)
-c.insert(16)
-c.insert(6)
-
-
-print(c.list(59))
-print(c.list(17))
